sonastream/core

split -> payout address variable rename

contracts/SonaRewardToken.sol
@@ -77,13 +77,13 @@ contract SonaRewardToken is SonaMinter, ISonaRewardToken {
 	/// @param _collector The address that won the auction
 	/// @param _artistTxId the arweave txId of the artist edition bundle contained on arweave
 	/// @param _collectorTxId the arweave txId of the collector edition bundle contained on arweave
-	/// @param _splits the address to distribute funds to, potentially to split them with collaborators
-	function mintFromAuction(uint256 _tokenId, address _artist, address _collector, string calldata _artistTxId, string calldata _collectorTxId, address payable _splits) external onlySonaMinter {
+	/// @param _payout the address to distribute funds to, potentially to split them with collaborators
+	function mintFromAuction(uint256 _tokenId, address _artist, address _collector, string calldata _artistTxId, string calldata _collectorTxId, address payable _payout) external onlySonaMinter {
 		if (_tokenId % 2 == 0) revert SonaRewardToken_ArtistEditionEven();
 		if (AddressableTokenId.getAddress(_tokenId) != _artist) revert SonaRewardToken_NoArtistInTokenId();
 		uint256 artistTokenId = _tokenId.getArtistEdition();
 		_mint(_artist, artistTokenId);
-		_setTokenMetadata(artistTokenId, _artistTxId, _splits);
+		_setTokenMetadata(artistTokenId, _artistTxId, _payout);
 
 		_mint(_collector, _tokenId);
 		_setTokenMetadata(_tokenId, _collectorTxId, payable(address(0)));
@@ -120,8 +120,8 @@ contract SonaRewardToken is SonaMinter, ISonaRewardToken {
 
 	/// @dev Returns the splits address of the RewardToken
 	/// @param _tokenId The ID of the token to fetch
-	function getRewardTokenSplitsAddr(uint256 _tokenId) external view checkExists(_tokenId) returns (address payable splits) {
-		return rewardTokens[_tokenId].splits;
+	function getRewardTokenSplitsAddr(uint256 _tokenId) external view checkExists(_tokenId) returns (address payable payout) {
+		return rewardTokens[_tokenId].payout;
 	}
 
 	/// @notice Check if token `_tokenId` exists
@@ -140,9 +140,9 @@ contract SonaRewardToken is SonaMinter, ISonaRewardToken {
 		emit RewardTokenArweaveTxIdUpdated({ tokenId: _tokenId, txId: _txId });
 	}
 
-	function _setTokenMetadata(uint256 _tokenId, string calldata _txId, address payable _splits) internal {
-		rewardTokens[_tokenId] = RewardToken({ arTxId: _txId, splits: _splits });
+	function _setTokenMetadata(uint256 _tokenId, string calldata _txId, address payable _payout) internal {
+		rewardTokens[_tokenId] = RewardToken({ arTxId: _txId, payout: _payout });
 
-		emit RewardTokenMetadataUpdated({ tokenId: _tokenId, txId: _txId, splits: _splits });
+		emit RewardTokenMetadataUpdated({ tokenId: _tokenId, txId: _txId, payout: _payout });
 	}
 }

contracts/interfaces/ISonaRewardToken.sol
@@ -35,7 +35,7 @@ interface ISonaRewardToken {
 	/// @dev Emitted when a RewardToken's metadata is changed.
 	/// @param tokenId The id of the token
 	/// @param txId The cid of the RewardToken
-	event RewardTokenMetadataUpdated(uint256 indexed tokenId, string txId, address splits);
+	event RewardTokenMetadataUpdated(uint256 indexed tokenId, string txId, address payout);
 
 	/// @dev Emitted when a RewardToken is removed.
 	/// @param tokenId The id of the RewardToken
@@ -54,15 +54,15 @@ interface ISonaRewardToken {
 	struct RewardToken {
 		/// @dev The hash of the Arweave transaction where the metadata is stored.
 		string arTxId;
-		/// @dev The address of a splits contract for sharing rewards with collaborators
-		address payable splits;
+		/// @dev The address for sharing rewards with collaborators
+		address payable payout;
 	}
 
 	/*//////////////////////////////////////////////////////////////
 	/                            FUNCTIONS
 	//////////////////////////////////////////////////////////////*/
 
-	function mintFromAuction(uint256 _tokenId, address _artist, address _collector, string memory _artistCid, string memory _collectorCid, address payable _splits) external;
+	function mintFromAuction(uint256 _tokenId, address _artist, address _collector, string memory _artistCid, string memory _collectorCid, address payable _payout) external;
 
 	function updateArweaveTxId(uint256 _tokenId, string calldata _TxId) external;
 
@@ -72,7 +72,7 @@ interface ISonaRewardToken {
 
 	function getRewardTokenMetadata(uint256 _tokenId) external view returns (RewardToken memory metadata);
 
-	function getRewardTokenSplitsAddr(uint256 _tokenId) external view returns (address payable splits);
+	function getRewardTokenSplitsAddr(uint256 _tokenId) external view returns (address payable payout);
 
 	function initialize(string memory _name, string memory _symbol, address _eoaAdmin, address _minter) external;
 

topocount/sona-protocol

contracts/SonaRewardToken.sol
@@ -77,13 +77,13 @@ contract SonaRewardToken is SonaMinter, ISonaRewardToken {
 	/// @param _collector The address that won the auction
 	/// @param _artistTxId the arweave txId of the artist edition bundle contained on arweave
 	/// @param _collectorTxId the arweave txId of the collector edition bundle contained on arweave
-	/// @param _splits the address to distribute funds to, potentially to split them with collaborators
-	function mintFromAuction(uint256 _tokenId, address _artist, address _collector, string calldata _artistTxId, string calldata _collectorTxId, address payable _splits) external onlySonaMinter {
+	/// @param _payout the address to distribute funds to, potentially to split them with collaborators
+	function mintFromAuction(uint256 _tokenId, address _artist, address _collector, string calldata _artistTxId, string calldata _collectorTxId, address payable _payout) external onlySonaMinter {
 		if (_tokenId % 2 == 0) revert SonaRewardToken_ArtistEditionEven();
 		if (AddressableTokenId.getAddress(_tokenId) != _artist) revert SonaRewardToken_NoArtistInTokenId();
 		uint256 artistTokenId = _tokenId.getArtistEdition();
 		_mint(_artist, artistTokenId);
-		_setTokenMetadata(artistTokenId, _artistTxId, _splits);
+		_setTokenMetadata(artistTokenId, _artistTxId, _payout);
 
 		_mint(_collector, _tokenId);
 		_setTokenMetadata(_tokenId, _collectorTxId, payable(address(0)));
@@ -120,8 +120,8 @@ contract SonaRewardToken is SonaMinter, ISonaRewardToken {
 
 	/// @dev Returns the splits address of the RewardToken
 	/// @param _tokenId The ID of the token to fetch
-	function getRewardTokenSplitsAddr(uint256 _tokenId) external view checkExists(_tokenId) returns (address payable splits) {
-		return rewardTokens[_tokenId].splits;
+	function getRewardTokenPayoutAddr(uint256 _tokenId) external view checkExists(_tokenId) returns (address payable payout) {
+		return rewardTokens[_tokenId].payout;
 	}
 
 	/// @notice Check if token `_tokenId` exists
@@ -140,9 +140,9 @@ contract SonaRewardToken is SonaMinter, ISonaRewardToken {
 		emit RewardTokenArweaveTxIdUpdated({ tokenId: _tokenId, txId: _txId });
 	}
 
-	function _setTokenMetadata(uint256 _tokenId, string calldata _txId, address payable _splits) internal {
-		rewardTokens[_tokenId] = RewardToken({ arTxId: _txId, splits: _splits });
+	function _setTokenMetadata(uint256 _tokenId, string calldata _txId, address payable _payout) internal {
+		rewardTokens[_tokenId] = RewardToken({ arTxId: _txId, payout: _payout });
 
-		emit RewardTokenMetadataUpdated({ tokenId: _tokenId, txId: _txId, splits: _splits });
+		emit RewardTokenMetadataUpdated({ tokenId: _tokenId, txId: _txId, payout: _payout });
 	}
 }

contracts/interfaces/ISonaRewardToken.sol
@@ -35,7 +35,7 @@ interface ISonaRewardToken {
 	/// @dev Emitted when a RewardToken's metadata is changed.
 	/// @param tokenId The id of the token
 	/// @param txId The cid of the RewardToken
-	event RewardTokenMetadataUpdated(uint256 indexed tokenId, string txId, address splits);
+	event RewardTokenMetadataUpdated(uint256 indexed tokenId, string txId, address payout);
 
 	/// @dev Emitted when a RewardToken is removed.
 	/// @param tokenId The id of the RewardToken
@@ -54,15 +54,15 @@ interface ISonaRewardToken {
 	struct RewardToken {
 		/// @dev The hash of the Arweave transaction where the metadata is stored.
 		string arTxId;
-		/// @dev The address of a splits contract for sharing rewards with collaborators
-		address payable splits;
+		/// @dev The address for sharing rewards with collaborators
+		address payable payout;
 	}
 
 	/*//////////////////////////////////////////////////////////////
 	/                            FUNCTIONS
 	//////////////////////////////////////////////////////////////*/
 
-	function mintFromAuction(uint256 _tokenId, address _artist, address _collector, string memory _artistCid, string memory _collectorCid, address payable _splits) external;
+	function mintFromAuction(uint256 _tokenId, address _artist, address _collector, string memory _artistCid, string memory _collectorCid, address payable _payout) external;
 
 	function updateArweaveTxId(uint256 _tokenId, string calldata _TxId) external;
 
@@ -72,7 +72,7 @@ interface ISonaRewardToken {
 
 	function getRewardTokenMetadata(uint256 _tokenId) external view returns (RewardToken memory metadata);
 
-	function getRewardTokenSplitsAddr(uint256 _tokenId) external view returns (address payable splits);
+	function getRewardTokenPayoutAddr(uint256 _tokenId) external view returns (address payable payout);
 
 	function initialize(string memory _name, string memory _symbol, address _eoaAdmin, address _minter) external;
 