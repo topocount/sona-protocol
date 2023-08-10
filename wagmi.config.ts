4814b8e846b652b989ef31cb8949e6254a57a0a9
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2023-08-10 10:49:13 -0500

    Remove goerli support
    The current ABI's do not support goerli

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -7,15 +7,12 @@ export default defineConfig({
 		foundry({
 			deployments: {
 				SonaRewardToken: {
-					5: "0x08aCA4dCb070a2Ac8c4D89eD521eC9F9a69B43F0",
 					11155111: "0x017CFf85774064F096a37214A1b3a423732932B3",
 				},
 				SonaReserveAuction: {
-					5: "0x683AadCC902d601B54DECb59F59d140FfE27F036",
 					11155111: "0x9c2ad06D8379eD690bFc5B32DF6c1FF8d20e8f32",
 				},
 				SonaRewards: {
-					5: "0x4a1392dCc64824cCE63a1756Bd6b676Ba8954092",
 					11155111: "0x670478ED5a94A81C5295731Edf5D6A3AFa41fb71",
 				},
 			},
