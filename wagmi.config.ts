941cbad0072be2a72227642443c39db84aa8f182
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2024-02-22 09:51:13 -0600

    feat: add generic abis to package exports

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from "@wagmi/cli"
-import { foundry } from "@wagmi/cli/plugins"
+import { erc, foundry } from "@wagmi/cli/plugins"
 import deployments from "./script/deployments"
 
 export default defineConfig({
-	out: "abi/generated.ts",
-	plugins: [
-		foundry({
-			deployments,
-			artifacts: "dist/artifacts",
-			include: ["Sona*.sol/Sona*.json"],
-			forge: {
-				build: false,
-			},
-		}),
-	],
+  out: "abi/generated.ts",
+  plugins: [
+    foundry({
+      deployments,
+      artifacts: "dist/artifacts",
+      include: ["Sona*.sol/Sona*.json"],
+      forge: {
+        build: false,
+      },
+    }),
+    erc({
+      20: true,
+      721: true,
+      4626: false,
+    }),
+  ],
 })
