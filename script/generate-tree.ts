020c0a05a7b499ec63431ae02e69e246b9035780
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2024-02-22 09:50:21 -0600

    chore: eslint formatting

--- script/generate-tree.ts
+++ script/generate-tree.ts
@@ -18,6 +18,7 @@ let tree = StandardMerkleTree.of(values, [
 ])
 
 // (3)
+console.log("\n\ntree 1")
 console.log("Merkle Root:", tree.root)
 
 console.log("leaves: ", Array.from(tree.entries()))
@@ -26,15 +27,18 @@ console.log("leaf: ", tree.leafHash(Array.from(tree.entries())[1][1]))
 console.log("proof ", tree.getProof(1))
 console.log("tree ", tree.render())
 // (4)
-fs.writeFileSync("script/tree.json", JSON.stringify(tree.dump(), null, 2))
+//fs.writeFileSync("script/tree.json", JSON.stringify(tree.dump(), null, 2))
 
 console.log("\n\ntree 2")
 // (1)
 values = [
 	// [tokenId								, amount, 			,start, end]
-	["0x1", "10000000000000000000", 1, 2],
-	["0x2", "5000000000000000000", 1, 2],
-	["0x3", "2500000000000000000", 1, 2],
+	["0x1", "5000000000000000000", 0, 1],
+	["0x2", "2500000000000000000", 0, 1],
+	["0x3", "1250000000000000000", 0, 1],
+	//["0x1", "10000000000000000000", 1, 2],
+	//["0x2", "5000000000000000000", 1, 2],
+	//["0x3", "2500000000000000000", 1, 2],
 ]
 
 // (2)
@@ -54,4 +58,4 @@ console.log("leafhash: ", tree.leafHash(Array.from(tree.entries())[1][1]))
 console.log("proof ", tree.getProof(1))
 console.log("tree ", tree.render())
 // (4)
-fs.writeFileSync("script/tree2.json", JSON.stringify(tree.dump(), null, 2))
+//fs.writeFileSync("script/tree2.json", JSON.stringify(tree.dump(), null, 2))
